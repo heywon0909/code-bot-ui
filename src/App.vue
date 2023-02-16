d5234d4aa526bfd51daa063caf800378e12f323a
heywon0909 <hw7319@naver.com>
2023-02-16 23:10:25 +0900

    코딩봇 ui 작업중...(tailwind 입히기)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+  <div class="bg-slate-800 w-2/3 h-screen rounded-md">
+    <div class="text-sky-500 font-medium w-full p-2 h-10 border-b-2">CodeBot</div>
+    <div class="flex w-full h-auto text-white p-2 mt-10 bg-gray-700">
+       <div>
+         <img src="./assets/image/user.png" class="w-10"/>
+       </div>
+       <div class="p-2">
+        sdfsdfasdasd
+       </div>
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+   
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ChatFrame.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white m-auto w-1/6">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
