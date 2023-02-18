184aa79b21ea5c65c22c8b6476cf3d8a76c3f099
heywon0909 <hw7319@naver.com>
2023-02-18 15:27:40 +0900

    tailwind css 작업중..

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-<div class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
-  <div class="bg-slate-800 w-2/3 h-screen rounded-md">
-    <div class="text-sky-500 font-medium w-full p-2 h-10 border-b-2">CodeBot</div>
-    <div class="flex w-full h-auto text-white p-2 mt-10 bg-gray-700">
-       <div>
-         <img src="./assets/image/user.png" class="w-10"/>
-       </div>
-       <div class="p-2">
-        sdfsdfasdasd
-       </div>
+  <div
+    class="flex flex-col items-center justify-center w-screen h-screen bg-gray-100 text-gray-800 p-10"
+  >
+    <div class="bg-slate-800 w-2/3 rounded-md h-screen">
+      <div class="text-sky-500 text-xl font-semibold w-full p-2 h-12 border-b-2">
+        CodeBot
+      </div>
+      <div class="h-3/4">
+      <div class="flex w-full text-white p-2  bg-gray-700 h-max">
+        <div class="flex-auto w-1/4">
+          <img src="./assets/image/profile.png" class="w-10" />
+        </div>
+        <div class="flex-auto w-3/4 break-all">
+         311312312321312312321321312313123312312313312312332131212313112312312312312123131sdfsdfasdasd
+        </div>
+      </div>
+      <div class="flex w-full h-auto text-white p-2 bg-gray-700 h-max">
+        <div class="flex-auto w-3/4 break-all">
+         311312312321312312321321312313123312312313312312332131212313112312312312312123131sdfsdfasdasd
+        </div>
+         <div class="flex-auto w-1/4">
+          <img src="./assets/image/robot.png" class="w-10" />
+        </div>
+      </div>
+      </div>
+      <div class="flex bg-white w-full h-20">
+        <input type="text"/>
+        <button>submit</button>
+      </div>
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-    
-   
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-</style>
+<style></style>
