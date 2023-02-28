620923f71f7605a785214febe6847333ad76d7fa
heywon0909 <hw7319@naver.com>
2023-02-28 10:01:03 +0900

    로컬 서버로 대체

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: 'https://code-bot-ivzc.onrender.com/'
-  }
+  // devServer: {
+  //   proxy: 'https://code-bot-ivzc.onrender.com/'
+  // }
 })
